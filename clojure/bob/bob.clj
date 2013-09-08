b370d8831e8657d3cbfa2ff3f5132f3b68c9f9a7
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-08 17:45:43 +0200

    Move predicates into defns in bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,7 +1,17 @@
 (ns bob)
+
+(defn silence? [sentence]
+    (clojure.string/blank? sentence))
+
+(defn shouting? [sentence]
+    (= sentence (clojure.string/upper-case sentence)))
+
+(defn question? [sentence]
+    (= (last sentence) \?))
+
 (defn response-for [sentence]
   (cond
-    (clojure.string/blank? sentence) "Fine. Be that way!"
-    (= sentence (clojure.string/upper-case sentence)) "Woah, chill out!"
-    (= (last sentence) \?) "Sure."
+    (silence? sentence) "Fine. Be that way!"
+    (shouting? sentence) "Woah, chill out!"
+    (question? sentence) "Sure."
     :else "Whatever."))
