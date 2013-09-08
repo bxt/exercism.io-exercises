35ca8a48c4bbb488123c07c6937062c9e6039772
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-08 16:48:07 +0200

    Make use of cond in bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,9 +1,7 @@
 (ns bob)
 (defn response-for [sentence]
-  (if (clojure.string/blank? sentence)
-    "Fine. Be that way!"
-    (if (= sentence (clojure.string/upper-case sentence))
-      "Woah, chill out!"
-      (if (= (last sentence) \?)
-        "Sure."
-        "Whatever."))))
+  (cond
+    (clojure.string/blank? sentence) "Fine. Be that way!"
+    (= sentence (clojure.string/upper-case sentence)) "Woah, chill out!"
+    (= (last sentence) \?) "Sure."
+    :else "Whatever."))
