c04ffba9f9948aaa0a3929f0ec85899771f04d2b
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-12 14:33:26 +0200

    Choose re.finditer over re.findall for wordcount.py

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -6,4 +6,4 @@ class Phrase(object):
     self._string = string
   
   def word_count(self):
-    return Counter(re.findall(r'\w+',self._string.lower()))
+    return Counter(m.group(0) for m in re.finditer(r'\w+',self._string.lower()))
