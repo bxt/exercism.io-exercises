77fb97d51d3c746f12a52d36de733bdbda6accc3
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-15 16:27:11 +0200

    Remove unused private def from anagram.clj

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,8 +1,6 @@
 (ns anagram
   (:require [clojure.string :as s]))
 
-(defn- normalize [word] (-> word s/lower-case sort))
-
 (defn- anagram? [& words]
   (let [lowers (map s/lower-case words)
         freqs (map frequencies lowers)]
