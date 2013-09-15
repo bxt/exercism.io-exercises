3d02479b11eb3b7ea2fc31a5c4cf8903323d0f88
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-15 23:45:44 +0200

    Directly choose random Chars in Robot.hs (and do not chr Ints)

--- haskell/robot-name/Robot.hs
+++ haskell/robot-name/Robot.hs
@@ -4,7 +4,6 @@ module Robot
   , resetName
   ) where
 
-import Data.Char (chr)
 import Data.IORef
 import System.Random (randomRIO)
 
@@ -17,11 +16,11 @@ mkRobot :: IO Robot
 mkRobot = robotLabel >>= newIORef
 
 robotLabel :: IO String
-robotLabel = mapM (fmap chr) [char, char, num, num, num]
+robotLabel = sequence [chr, chr, num, num, num]
 
-char, num :: IO Int
-char = randomRIO (65,90)
-num  = randomRIO (48,57)
+chr, num :: IO Char
+chr = randomRIO ('A','Z')
+num = randomRIO ('0','9')
 
 resetName :: Robot -> IO ()
 resetName r = robotLabel >>= writeIORef r
