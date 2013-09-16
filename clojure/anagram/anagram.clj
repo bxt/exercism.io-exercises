1fd390a97d86c1c8429aa645d585cca2a87ffe06
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-16 22:49:25 +0200

    Introduce a cache dict in anagram.clj

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,15 +1,19 @@
 (ns anagram
   (:require [clojure.string :as s]))
 
-(defn- normalizations [word]
-  (rest (reductions #(%2 %1) [word s/lower-case frequencies])))
+(defn- on [g f x y] (g (f x) (f y)))
+
+(defn- noralize [word]
+  (let [[lower (s/lower-case word)]
+        [freqs (frequencies lower)]]
+        {:lower lower :freqs freqs}))
 
 (defn- anagram-of? [word1]
-  (let [[word1 w1freq] (normalizations word1)]
+  (let [n1 (noralize word1)]
     (fn [word2]
-      (let [[word2 w12req] (normalizations word2)]
-        (and (not= word1 word2)
-             (   = w1freq w12req))))))
+      (let [n2 (noralize word2)]
+        (and (on not= :lower n1 n2)
+             (on =    :freqs n1 n2))))))
 
 (defn anagrams-for [word candidates]
   (filter (anagram-of? word) candidates))
