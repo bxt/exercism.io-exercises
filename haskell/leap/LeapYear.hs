de452a2d2ff05f79ad0ad549685bb14b4ad97d65
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-17 11:27:29 +0200

    Add solution for LeapYear.hs

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -2,6 +2,12 @@ module LeapYear
   ( isLeapYear
   ) where
 
-
 isLeapYear :: Int -> Bool
-isLeapYear = undefined
+isLeapYear y
+  | divisible 400 y = True
+  | divisible 100 y = False
+  | divisible   4 y = True
+  | otherwise       = False
+
+divisible :: Int -> Int -> Bool
+divisible by n = rem n by == 0
