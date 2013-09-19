f30ee4650ce7a63192e5d96786b430adb1dd6b29
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-19 02:34:59 +0200

    Add missing type signature to ETL.hs

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -13,4 +13,5 @@ transform :: Map Int [String] -> Map String Int
 transform = Map.fromList . transformList . Map.toList
   where transformList = concatMap (uncurry stringsToEntries)
 
+stringsToEntries :: Int -> [String] -> [(String,Int)]
 stringsToEntries points = map ((,points) . map toLower)
