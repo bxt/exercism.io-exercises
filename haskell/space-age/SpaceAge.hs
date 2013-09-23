bade60f7e602226a963f028b439c23cea049c3a0
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-23 23:30:17 +0200

    Get rid of repeated orbitalPeriod in SpaceAge.hs

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -13,11 +13,12 @@ toEarthYears :: Integer -> Float
 toEarthYears = (/ 31557600) . fromIntegral
 
 orbitalPeriod :: Planet -> Float
-orbitalPeriod Mercury =   0.2408467
-orbitalPeriod Venus   =   0.61519726
-orbitalPeriod Earth   =   1.0
-orbitalPeriod Mars    =   1.8808158
-orbitalPeriod Jupiter =  11.862615
-orbitalPeriod Saturn  =  29.447498
-orbitalPeriod Uranus  =  84.016846
-orbitalPeriod Neptune = 164.79132
+orbitalPeriod p = case p of
+    Mercury ->   0.2408467
+    Venus   ->   0.61519726
+    Earth   ->   1.0
+    Mars    ->   1.8808158
+    Jupiter ->  11.862615
+    Saturn  ->  29.447498
+    Uranus  ->  84.016846
+    Neptune -> 164.79132
