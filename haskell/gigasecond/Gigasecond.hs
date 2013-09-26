d60896a2563c67d6ff8e7413414116c5703a880a
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-26 13:46:30 +0200

    Wrap additional definitions in a where clause in Gigasecond.hs

--- haskell/gigasecond/Gigasecond.hs
+++ haskell/gigasecond/Gigasecond.hs
@@ -6,9 +6,7 @@ import Data.Time.Calendar
 
 fromDay :: Day -> Day
 fromDay = addDays gigasecondsDays
-
-gigasecondsDays = giga `div` secondsPerDay
-
-secondsPerDay = 60 * 60 * 24
-
-giga = 10^9
+  where
+    gigasecondsDays = giga `div` secondsPerDay
+    secondsPerDay = 60 * 60 * 24
+    giga = 10^9
