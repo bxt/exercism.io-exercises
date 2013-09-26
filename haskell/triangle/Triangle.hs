753d3ba63c390a53a103370adf7ef6ea4af293be
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-26 23:14:28 +0200

    Add scaffold for Triangle.hs

---
+++ haskell/triangle/Triangle.hs
@@ -0,0 +1,10 @@
+module Triangle
+  ( TriangleType(..)
+  , triangleType
+  ) where
+
+data TriangleType = Equilateral | Isosceles | Scalene | Illogical
+  deriving (Show, Eq)
+
+triangleType :: Int -> Int -> Int -> TriangleType
+triangleType = undefined
