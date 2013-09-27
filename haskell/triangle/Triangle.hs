6ed5d3098684119e09092157149ce4e5eeb76b6a
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-27 22:38:43 +0200

    Fix some stuff at Triangle.hs

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -3,23 +3,20 @@ module Triangle
   , triangleType
   ) where
 
-import Control.Applicative
-
 data TriangleType = Equilateral | Isosceles | Scalene | Illogical
   deriving (Show, Eq)
 
 triangleType :: Int -> Int -> Int -> TriangleType
 triangleType a b c
   | 3 > countSides trangleEquation a b c = Illogical
-  | 3 == countSides (const (==)) a b c   = Equilateral
-  | 1 == countSides (const (==)) a b c   = Isosceles
+  | 3 == sideEqualities                  = Equilateral
+  | 1 == sideEqualities                  = Isosceles
   | otherwise                            = Scalene
+  where sideEqualities = countSides (const (==)) a b c
 
 trangleEquation :: Int -> Int -> Int -> Bool
 trangleEquation a b c = a + b > c
 
 countSides :: (Int -> Int -> Int -> Bool) -> Int -> Int -> Int -> Int
-countSides f a b c = length $ filter (uncurry3 f) [(a, b, c), (b, c, a), (c, a, b)]
+countSides f a b c = length $ filter id $ zipWith3 f [a, b, c] [b, c, a] [c, a, b]
 
-uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
-uncurry3 f (a,b,c) = f a b c
