f0a5f145697b2cf9a94aaa44080c3ebacddb9fb1
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-28 15:25:23 +0200

    Add a solution for Roman.hs

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -3,4 +3,22 @@ module Roman
   ) where
 
 numerals :: Int -> String
-numerals = undefined
+numerals = numerals' digits where
+  numerals' _ 0 = ""
+  numerals' x@((l,m):xs) n | n >= m    = l ++ numerals' x (n-m)
+                           | otherwise = numerals' xs n
+
+digits = [ ( "M",1000)
+         , ("CM", 900)
+         , ( "D", 500)
+         , ("CD", 400)
+         , ( "C", 100)
+         , ("XC",  90)
+         , ( "L",  50)
+         , ("XL",  40)
+         , ( "X",  10)
+         , ("IX",   9)
+         , ( "V",   5)
+         , ("IV",   4)
+         , ( "I",   1)
+         ]
