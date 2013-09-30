507222ca98445fe32b8991455afcac23dd091b13
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-30 13:36:54 +0200

    Precompute scores in Srabble.hs

--- haskell/scrabble-score/Scrabble.hs
+++ haskell/scrabble-score/Scrabble.hs
@@ -4,10 +4,13 @@ module Scrabble
   ) where
 
 import Data.Char (toUpper)
+import Data.Array.IArray
+import Data.Array.Unboxed
 
-scoreLetter :: Char -> Int
-scoreLetter = scoreUpperLetter . toUpper where
-  scoreUpperLetter l
+scores :: UArray Char Int
+scores = array r [(l, s l) | l <- range r] where
+  r = ('A','Z')
+  s l
     | l `elem` "AEIOULNRST" =  1
     | l `elem` "DG"         =  2
     | l `elem` "BCMP "      =  3
@@ -17,5 +20,9 @@ scoreLetter = scoreUpperLetter . toUpper where
     | l `elem` "QZ"         = 10
     | otherwise             = error $ l:" can not be scored"
 
+scoreLetter :: Char -> Int
+scoreLetter = (scores !) . toUpper where
+
 scoreWord :: String -> Int
 scoreWord = sum . map scoreLetter
+
