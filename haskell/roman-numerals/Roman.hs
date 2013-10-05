69b33c44c8c189f9df8f8cdaf0128d54c42fbc41
Bernhard Häussner <bxt@die-optimisten.net>
2013-10-05 11:50:11 +0200

    Fix some whitespace in Roman.hs

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -4,21 +4,20 @@ module Roman
 
 numerals :: Int -> String
 numerals = numerals' digits where
-  numerals' _ 0 = ""
-  numerals' x@((l,m):xs) n | n >= m    = l ++ numerals' x (n-m)
-                           | otherwise = numerals' xs n
-
-digits = [ ( "M",1000)
-         , ("CM", 900)
-         , ( "D", 500)
-         , ("CD", 400)
-         , ( "C", 100)
-         , ("XC",  90)
-         , ( "L",  50)
-         , ("XL",  40)
-         , ( "X",  10)
-         , ("IX",   9)
-         , ( "V",   5)
-         , ("IV",   4)
-         , ( "I",   1)
-         ]
+  numerals' _            0             = ""
+  numerals' x@((l,m):xs) n | n >= m    = l ++ numerals' x  (n-m)
+                           | otherwise =      numerals' xs n
+  digits = [ ( "M",1000)
+          , ("CM", 900)
+          , ( "D", 500)
+          , ("CD", 400)
+          , ( "C", 100)
+          , ("XC",  90)
+          , ( "L",  50)
+          , ("XL",  40)
+          , ( "X",  10)
+          , ("IX",   9)
+          , ( "V",   5)
+          , ("IV",   4)
+          , ( "I",   1)
+          ]
