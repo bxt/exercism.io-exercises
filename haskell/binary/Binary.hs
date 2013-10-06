ea37d2454d000bc3eb8192bd868be387e1ffbc20
Bernhard Häussner <bxt@die-optimisten.net>
2013-10-06 20:37:20 +0200

    Use strict version of foldl in Binary.hs

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -2,8 +2,10 @@ module Binary
   ( toDecimal
   ) where
 
+import Data.List (foldl')
+
 toDecimal :: String -> Integer
-toDecimal = foldl f 0 where
+toDecimal = foldl' f 0 where
   f n x = n * 2 + digit x
   digit '1' = 1
   digit  _  = 0
