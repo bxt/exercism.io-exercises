5e2602ac4bbef1ffd29d5bc55be277d0d9c248e2
Bernhard Häussner <bxt@die-optimisten.net>
2013-10-07 22:45:46 +0200

    Add working solution for PrimeFactors.hs

--- haskell/prime-factors/PrimeFactors.hs
+++ haskell/prime-factors/PrimeFactors.hs
@@ -3,4 +3,8 @@ module PrimeFactors
   ) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors = undefined
+primeFactors = primeFactors' 2 where
+  primeFactors' k n
+    | k*k > n        = if n > 1 then [n] else []
+    | n `mod` k == 0 = [k] ++ primeFactors'  k   (n `div` k)
+    | otherwise      =        primeFactors' (k+1) n
